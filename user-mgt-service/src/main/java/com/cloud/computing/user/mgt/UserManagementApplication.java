027942a986051caf43dfd7f069d98c76fbc51c60
thanujalk <thanuja@wso2.com>
2022-01-05 16:59:31 +0530

    Fix cors issue

--- user-mgt-service/src/main/java/com/cloud/computing/user/mgt/UserManagementApplication.java
+++ user-mgt-service/src/main/java/com/cloud/computing/user/mgt/UserManagementApplication.java
@@ -3,6 +3,10 @@ package com.cloud.computing.user.mgt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class UserManagementApplication {
@@ -10,6 +14,17 @@ public class UserManagementApplication {
     public static void main(final String[] args) {
 
         SpringApplication.run(UserManagementApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 
 }
